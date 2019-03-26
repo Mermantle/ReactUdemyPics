9e71335962443011fb0d6058041b94458db241d7
mermantle <mermantle@gmail.com>
2019-03-26 14:29:58 +0000

    change from iMac

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,25 +1,25 @@
 import React from 'react';
-
+// new component
 class SearchBar extends React.Component {
-   state = { term: ''};
+    state = { term: '' };
 
-   // we do not provide parenthesis to call back function 'onInputChange' to any eventHandler like onChange as 
-   // it would run each time render is called - so without it will only run
-   // when input text has changed - this is a comment problem 
-    render () {
+    // we do not provide parenthesis to call back function 'onInputChange' to any eventHandler like onChange as 
+    // it would run each time render is called - so without it will only run
+    // when input text has changed - this is a comment problem 
+    render() {
         return (
-        <div className="ui segment">
-            <form className="ui form">
-                <div className="field" >
-                    <label>Image Search</label>
-                    <input 
-                        type="text" 
-                        value={this.state.term} 
-                        onChange={(e) => this.setState({ term: e.target.value })} 
-                    />
-                </div>
-            </form>
-        </div>
+            <div className="ui segment">
+                <form className="ui form">
+                    <div className="field" >
+                        <label>Image Search</label>
+                        <input
+                            type="text"
+                            value={this.state.term}
+                            onChange={(e) => this.setState({ term: e.target.value })}
+                        />
+                    </div>
+                </form>
+            </div>
         );
     }
 }
