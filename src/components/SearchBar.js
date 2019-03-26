cd9da2453cd02c9e3669a96cb1d4a719a2f618b0
mermantle <mermantle@gmail.com>
2019-03-26 13:42:55 +0000

    tues am

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,11 +1,23 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
+   state = { term: ''};
+
+   // we do not provide parenthesis to call back function 'onInputChange' to any eventHandler like onChange as 
+   // it would run each time render is called - so without it will only run
+   // when input text has changed - this is a comment problem 
     render () {
         return (
-        <div>
-            <form>
-                <input type="text" />
+        <div className="ui segment">
+            <form className="ui form">
+                <div className="field" >
+                    <label>Image Search</label>
+                    <input 
+                        type="text" 
+                        value={this.state.term} 
+                        onChange={(e) => this.setState({ term: e.target.value })} 
+                    />
+                </div>
             </form>
         </div>
         );
